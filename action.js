9152614fd88e483507d9810cd276ee9d88bb3caf
Zarah-563 <98961128+Zarah-563@users.noreply.github.com>
2022-02-03 20:18:00 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,20 @@
+function ChangeToRed() {
+    document.body.style.backgroundColor = "red";
+    document.getElementsByClassName("child1");
+    innerhtml = "red";
+}
+
+function ChangeToGreen() {
+    document.body.style.backgroundColor = "green";
+    document.getElementsByClassName("child2");
+}
+
+function ChangeToIndigo() {
+    document.body.style.backgroundColor = "indigo"
+    document.getElementsByClassName("child3");
+}
+
+function ChangeToYellow() {
+    document.body.style.backgroundColor = "yellow";
+    document.getElementsByClassName("child4");
+}
